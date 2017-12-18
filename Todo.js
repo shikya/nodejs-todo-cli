dc33fc56582056a4bcfd337491dc51473185906c
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-18 16:04:04 +0530

    Added functions

---
+++ Todo.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const writeTodos = (todos) => {
+    fs.writeFileSync("data.json", JSON.stringify(todos));
+    return todos;
+};
+
+const getTodos = () => {
+    var todos = [];
+    try {
+        todos = fs.readFileSync("data.json").toString();
+        todos = JSON.parse(todos);
+    } catch (e) {
+        fs.writeFileSync("data.json", "[]");
+        todos = [];
+    }
+    return todos;
+};
+
+const appendTodo = (todo) => {
+    var todos = this.getTodos();
+    const isPreset = (todos.filter(interator => interator.title === todo.title)).length > 0;
+    if(isPreset)
+        console.log("Already Present");
+    else{
+        todos.push(todo);
+        todos = writeTodos(todos);        
+    }
+    return todos;
+};
+
+const deleteTodo = (title) => {
+    var todos = getTodos();
+    var filteredTodos = todos.filter(singleTodo => title !== singleTodo.title);
+    writeTodos(filteredTodos);
+    return filteredTodos;
+};
+
+const getTodo = (title) => {
+    debugger;
+    const todos = getTodos();
+    var filteredTodos = todos.filter(singleTodo => title == singleTodo.title);
+    return filteredTodos[0];
+}
+
+module.exports = {
+    getTodos,
+    appendTodo,
+    deleteTodo,
+    getTodo
+};
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// 3rd Party Library Imports
+const argv = require('yargs').argv
+// Own Library Imports
+const Todo = require('./Todo.js');
+
+console.log(argv);
+
+if(argv._[0] === 'add'){
+    Todo.appendTodo({'tittle': argv.title, description: argv.desc});
+} else if(argv._[0] === 'remove') {
+    Todo.deleteTodo(Todo.deleteTodo(argv.title));
+} else if(argv._[0] === 'view') {
+    console.log(Todo.getTodo(argv.title));
+} else if(argv._[0] === 'all') {
+    console.log(Todo.getTodos());
+}
