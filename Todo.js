94e6debdf87d8d6c96ac7efe018b8e2e48a5940e
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-18 17:07:17 +0530

    Integrated yargs

--- Todo.js
+++ Todo.js
@@ -18,7 +18,8 @@ const getTodos = () => {
 };
 
 const appendTodo = (todo) => {
-    var todos = this.getTodos();
+    debugger;
+    var todos = getTodos();
     const isPreset = (todos.filter(interator => interator.title === todo.title)).length > 0;
     if(isPreset)
         console.log("Already Present");
@@ -37,15 +38,21 @@ const deleteTodo = (title) => {
 };
 
 const getTodo = (title) => {
-    debugger;
     const todos = getTodos();
     var filteredTodos = todos.filter(singleTodo => title == singleTodo.title);
     return filteredTodos[0];
+}
+
+const prittyPrint = todo => {
+    if(todo){
+        console.log(`\nTitle: ${todo.title}\nBody: ${todo.body}`);        
+    }
 }
 
 module.exports = {
     getTodos,
     appendTodo,
     deleteTodo,
-    getTodo
+    getTodo,
+    prittyPrint
 };
--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-// 3rd Party Library Imports
-const argv = require('yargs').argv
 // Own Library Imports
 const Todo = require('./Todo.js');
 
-console.log(argv);
+// console.log(argv);
+const argv = require('yargs')
+    .command('add', "Add TODO to database",{
+        title: {
+            describe: 'Title of TODO',
+            demand: true,
+            alias: 't'
+        },
+        body: {
+            describe: 'Body of TODO',
+            demand: false,
+            alias: 'b'
+        }
+    })
+    .command('all', "List add TODOS")
+    .command('remove', "Remove an existing TODO", {
+        title: {
+            describe: 'Title of TODO',
+            demand: true,
+            alias: 't'
+        }
+    })
+    .command('view', "View a single TODO", {
+        title: {
+            describe: 'Title of TODO',
+            demand: true,
+            alias: 't'
+        }
+    })
+    .help('h')
+    .argv;
 
 if(argv._[0] === 'add'){
-    Todo.appendTodo({'tittle': argv.title, description: argv.desc});
+    const todo = Todo.appendTodo({'title': argv.title, 'body': argv.body});
+    if(todo && todo.title) Todo.prittyPrint(todo);
 } else if(argv._[0] === 'remove') {
     Todo.deleteTodo(Todo.deleteTodo(argv.title));
+    console.log("Todo has been deleted");
 } else if(argv._[0] === 'view') {
-    console.log(Todo.getTodo(argv.title));
+    const todo = Todo.getTodo(argv.title);
+    Todo.prittyPrint(todo);
 } else if(argv._[0] === 'all') {
-    console.log(Todo.getTodos());
+    const todos = Todo.getTodos();
+    todos.forEach(singleTodo => {
+        Todo.prittyPrint(singleTodo);
+    });
+} else {
+    console.log("Invalid Option. Use -h for help");
 }
